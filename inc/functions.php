8b235e907c14e924d103796fa05c36d2a4c84ea6
JuusoPesonen <c2peju00@students.oamk.fi>
2023-04-06 14:09:36 +0300

    Added functions.php file

---
+++ inc/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function createDbConnection(){
+    $ini = parse_ini_file("../confi.ini", true);
+
+    $host = $ini["host"];
+    $db = $ini["db"];
+    $username = $ini["username"];
+    $pw = $ini["pw"];
+
+    try{
+        $dbcon = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $username, $pw);
+        return $dbcon;
+    }catch( PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+    return null;
+}
+
+function returnError(PDOException $pdoex) {
+    header('HTTP/1.1 500 Internal Server Error');
+    $error = array('error' => $pdoex->getMessage());
+    print json_encode($error);
+}
