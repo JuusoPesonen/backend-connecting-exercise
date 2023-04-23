d411d872e8872937d599694f85821ec1868babd9
JuusoPesonen <c2peju00@students.oamk.fi>
2023-04-23 15:29:55 +0300

    Added code for get_playlist.php

---
+++ get_playlist.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once "./inc/functions.php";
+require_once "./inc/headers.php";
+
+$conn = createDbConnection();
+
+$playlist_id = 1;
+
+$sql = "SELECT tracks.name as track_name, tracks.composer
+        FROM playlist_track
+        JOIN tracks ON tracks.TrackId = playlist_track.TrackId
+        WHERE playlist_track.PlaylistId = :playlist_id";
+
+/*"SELECT tracks.Name as track_name, artists.Name as composer
+        FROM playlist_track
+        JOIN tracks ON tracks.TrackId = playlist_track.TrackId
+        JOIN albums ON albums.AlbumId = tracks.AlbumId
+        JOIN artists ON artists.ArtistId = albums.ArtistId
+        WHERE playlist_track.PlaylistId = :playlist_id"; */
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':playlist_id', $playlist_id, PDO::PARAM_INT);
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($results as $result) {
+    echo "Track: " . $result['track_name'] . "<br>";
+    echo "Composer: " . $result['composer'] . "<br><br>";
+}
