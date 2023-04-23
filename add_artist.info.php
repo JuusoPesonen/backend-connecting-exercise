b581369ffe3d4a6f4b69f9ccaf62d0bfb85f0b32
JuusoPesonen <c2peju00@students.oamk.fi>
2023-04-23 15:31:03 +0300

    Added code for add_artist_info.php

---
+++ add_artist.info.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once "./inc/functions.php";
+require_once "./inc/headers.php";
+
+$conn = createDbConnection();
+
+$_POST['artist_name'] = "AC/DC";
+$_POST['album_title'] = "Back in Black";
+$_POST['tracks'] = array("Hells Bells", "Shoot to Thrill", "Back in Black");
+
+if (!isset($_POST['artist_name']) || !isset($_POST['album_title']) || !isset($_POST['tracks'])) {
+    echo "Error: Missing required data. Make sure to provide artist_name, album_title and tracks in the POST request.";
+    exit;
+}
+
+$artist_name = $_POST['artist_name'];
+$album_title = $_POST['album_title'];
+$tracks = $_POST['tracks']; // Array of track names
+
+try {
+    $conn->beginTransaction();
+
+    $stmt1 = $conn->prepare("INSERT INTO artists (Name) VALUES (:name)");
+    $stmt1->bindParam(':name', $artist_name, PDO::PARAM_STR);
+    $stmt1->execute();
+    $artist_id = $conn->lastInsertId();
+
+    $stmt2 = $conn->prepare("INSERT INTO albums (Title, ArtistId) VALUES (:title, :artist_id)");
+    $stmt2->bindParam(':title', $album_title, PDO::PARAM_STR);
+    $stmt2->bindParam(':artist_id', $artist_id, PDO::PARAM_INT);
+    $stmt2->execute();
+    $album_id = $conn->lastInsertId();
+
+    $stmt3 = $conn->prepare("INSERT INTO tracks (Name, AlbumId, MediaTypeId) VALUES (:name, :album_id, :media_type_id)");
+
+    foreach ($tracks as $track_name) {
+        $media_type_id = 1; // Replace this with the correct MediaTypeId
+        $stmt3->bindParam(':name', $track_name, PDO::PARAM_STR);
+        $stmt3->bindParam(':album_id', $album_id, PDO::PARAM_INT);
+        $stmt3->bindParam(':media_type_id', $media_type_id, PDO::PARAM_INT);
+        $stmt3->execute();
+    }
+
+    $conn->commit();
+    echo "Artist, album and tracks added successfully";
+} catch (Exception $e) {
+    $conn->rollBack();
+    echo "Error adding artist, album and tracks: " . $e->getMessage();
+}
