76549211017ab93da52dde58dad2221c4e7b22e2
JuusoPesonen <c2peju00@students.oamk.fi>
2023-04-23 15:46:40 +0300

    Added comments for add_artist_info.php code

--- add_artist.info.php
+++ add_artist.info.php
@@ -5,15 +5,18 @@ require_once "./inc/headers.php";
 
 $conn = createDbConnection();
 
+// Define artist, album and tracks data
 $_POST['artist_name'] = "AC/DC";
 $_POST['album_title'] = "Back in Black";
 $_POST['tracks'] = array("Hells Bells", "Shoot to Thrill", "Back in Black");
 
+// Check if all required data are present
 if (!isset($_POST['artist_name']) || !isset($_POST['album_title']) || !isset($_POST['tracks'])) {
     echo "Error: Missing required data. Make sure to provide artist_name, album_title and tracks in the POST request.";
     exit;
 }
 
+// Extract artist, album and tracks data from the POST request
 $artist_name = $_POST['artist_name'];
 $album_title = $_POST['album_title'];
 $tracks = $_POST['tracks'];
@@ -21,17 +24,20 @@ $tracks = $_POST['tracks'];
 try {
     $conn->beginTransaction();
 
+    // Insert new artist data to the database
     $stmt1 = $conn->prepare("INSERT INTO artists (Name) VALUES (:name)");
     $stmt1->bindParam(':name', $artist_name, PDO::PARAM_STR);
     $stmt1->execute();
     $artist_id = $conn->lastInsertId();
 
+    // Insert new album data to the database
     $stmt2 = $conn->prepare("INSERT INTO albums (Title, ArtistId) VALUES (:title, :artist_id)");
     $stmt2->bindParam(':title', $album_title, PDO::PARAM_STR);
     $stmt2->bindParam(':artist_id', $artist_id, PDO::PARAM_INT);
     $stmt2->execute();
     $album_id = $conn->lastInsertId();
 
+    // Insert new tracks data to the database
     $stmt3 = $conn->prepare("INSERT INTO tracks (Name, AlbumId, MediaTypeId) VALUES (:name, :album_id, :media_type_id)");
 
     foreach ($tracks as $track_name) {
